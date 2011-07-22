44697bdd8b0791b250779d6a1d4dfec05fbd7dc4
Andre Louis Caron <andre.louis.caron@usherbrooke.ca>
2011-07-22 22:30:40 +0000

    First working draft.

---
+++ code/netstring.c
@@ -0,0 +1,134 @@
+/* Copyright(c) Andre Caron <andre.l.caron@gmail.com>, 2011
+**
+** This document is covered by the an Open Source Initiative approved license. A
+** copy of the license should have been provided alongside this software package
+** (see "LICENSE.txt"). If not, terms of the license are available online at
+** "http://www.opensource.org/licenses/mit". */
+
+/*!
+ * @file netstring.cpp
+ * @author Andre Caron <andre.l.caron@gmail.com>
+ * @brief Parser for netstrings (http://cr.yp.to/proto/netstrings.txt).
+ */
+
+#include "cnetstring.h"
+
+#include <ctype.h>
+
+  /* Allow definition in build script. */
+#ifndef NETSTRING_MAXIMUM_LENGTH
+#   define NETSTRING_MAXIMUM_LENGTH (1*1024)
+#endif
+
+static const char * netstring_error_messages[] =
+{
+    "so far, so good",
+    "string too long",
+    "expected digit",
+};
+
+static size_t netstring_min ( size_t a, size_t b )
+{
+    return ((a < b)? a : b);
+}
+
+static int netstring_overflow
+    ( const struct netstring_limits * limits, size_t parsed )
+{
+    return (limits->maximum_length != 0)
+        && (parsed > limits->maximum_length);
+}
+
+const char * netstring_error_message ( enum netstring_parser_error error )
+{
+    return (netstring_error_messages[error]);
+}
+
+void netstring_setup
+    ( struct netstring_limits * limits, struct netstring_parser * parser )
+{
+        /* Default limits. */
+    limits->maximum_length = NETSTRING_MAXIMUM_LENGTH;
+        /* Default parser state. */
+    parser->state = netstring_parser_size;
+    parser->error = netstring_error_ok;
+    parser->parsed = 0;
+    parser->length = 0;
+    parser->accept = 0;
+    parser->object = 0;
+}
+
+void netstring_clear ( struct netstring_parser * parser )
+{
+    parser->state = netstring_parser_size;
+    parser->error = netstring_error_ok;
+    parser->parsed = 0;
+    parser->length = 0;
+}
+
+size_t netstring_consume ( const struct netstring_limits * limits,
+    struct netstring_parser * parser, const char * data, size_t size )
+{
+    int digit = 0;
+    char byte = 0;
+    size_t used = 0;
+    size_t parsed = parser->parsed;
+    size_t length = parser->length;
+    size_t count = 0;
+      /* String length appears as a prefix. */
+    if ( parser->state == netstring_parser_size )
+    {
+        for ( ; (used < size); ++used )
+        {
+            byte = data[used];
+              /* Check for length terminator. */
+            if ( byte == ':' )
+            {
+                ++used;
+                parser->state = netstring_parser_data;
+                break;
+            }
+              /* Make sure we get a digit. */
+            if ( !isdigit(byte) )
+            {
+                parser->error = netstring_error_nondigit;
+                parser->state = netstring_parser_fail;
+                break;
+            }
+              /* Update string length. */
+            length *= 10;
+            length += (byte - '0');
+              /* Stop parsing as soon as overflow is made possible. */
+            if (netstring_overflow(limits, length))
+            {
+                ++used;
+                parser->error = netstring_error_overflow;
+                parser->state = netstring_parser_fail;
+                break;
+            }
+        }
+    }
+      /* String data. */
+    if ( parser->state == netstring_parser_data )
+    {
+          /* Look as far ahead as possible. */
+        count = netstring_min(length-parsed, size-used);
+          /* Consume data. */
+        parser->accept(parser, data+used, count);
+          /* Update state. */
+        parsed += count;
+        used   += count;
+          /* Check if we've finished parsing. */
+        if ((parsed == length) && (data[used] == ','))
+        {
+            ++used;
+            parser->state = netstring_parser_done;
+            parser->finish(parser);
+        }
+    }
+      /* Update parser state. */
+    parser->parsed = parsed;
+    parser->length = length;
+      /* Report how many bytes were consumed. */
+    return (used);
+}
---
+++ code/netstring.h
@@ -0,0 +1,182 @@
+#ifndef _netstring_h__
+#define _netstring_h__
+
+/* Copyright(c) Andre Caron <andre.l.caron@gmail.com>, 2011
+**
+** This document is covered by the an Open Source Initiative approved license. A
+** copy of the license should have been provided alongside this software package
+** (see "LICENSE.txt"). If not, terms of the license are available online at
+** "http://www.opensource.org/licenses/mit". */
+
+/*!
+ * @file netstring.h
+ * @author Andre Caron <andre.l.caron@gmail.com>
+ * @brief Parser for netstrings (http://cr.yp.to/proto/netstrings.txt).
+ */
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /*!
+   * @brief Enumeration of error states the parser may report.
+   */
+enum netstring_parser_error
+{
+    netstring_error_ok = 0,
+    netstring_error_overflow,
+    netstring_error_nondigit,
+};
+
+  /*!
+   * @brief Gets a human-readable description of the error.
+   */
+const char * netstring_error_message ( enum netstring_parser_error error );
+
+  /*!
+   * @brief Enumeration of parser states.
+   */
+enum netstring_parser_state
+{
+      /*! @private
+       */
+    netstring_parser_size,
+      /*!
+       * @brief Length is known.  Check the parser's @a length field.
+       */
+    netstring_parser_data,
+      /*!
+       * @brief An error was detected.  Check the parser's @a error field.
+       */
+    netstring_parser_fail,
+      /*!
+       * @brief All data reported.
+       */
+    netstring_parser_done,
+};
+
+  /*!
+   * @brief Customizable limits for netstrings.
+   */
+struct netstring_limits
+{
+      /*!
+       * @brief Maximum admissible length for the netstring.
+       *
+       * @note This limit excludes the two characters used as delimiters.
+       */
+    size_t maximum_length;
+};
+
+  /*!
+   * @brief Netstring parser state.
+   *
+   * The parser is implemented as a Finite State Machine (FSM).  By itself, it
+   * does not buffer any data.  As soon as the syntax is validated, all content
+   * is forwarded to the client code through the callbacks.
+   */
+struct netstring_parser
+{
+      /*! @private
+       */
+    size_t parsed;
+
+      /*! @public
+       * @brief Length of the string, in bytes.
+       */
+    size_t length;
+
+      /*! @public
+       * @brief Current state of the parser.
+       *
+       * Client code should check the state after each call to
+       * @c netstring_consume() to check for important state transitions.  For
+       * instance, the total length of the string can be relied upon after the
+       * parser passes into the @c netstring_parser_data state.  Applications
+       * may use this hint to pre-allocate enough memory on the first call to
+       * @c accept().
+       *
+       * @warning This field is provided to clients as read-only.  Any attempt
+       *  to change it will cause unpredictable output.
+       */
+    enum netstring_parser_state state;
+
+      /*! @public
+       * @brief Last error reported by the parser.
+       *
+       * @warning This field should only be interpreted if @c state is set to
+       *  @c netstring_parser_fail.  Its value is undefined at all other times.
+       */
+    enum netstring_parser_error error;
+
+      /*! @public
+       * @brief Extra field for client code's use.
+       *
+       * The contents of this field are not interpreted in any way by the
+       * netstring request parser.  It is used for any purpose by client code.
+       * Usually, this field serves as link back to the owner object and used by
+       * registered callbacks.
+       */
+    void * object;
+
+      /*!
+       * @brief Callback supplying data for string contents.
+       * @param parser The netstring parser itself.  Useful for checking the
+       *  parser state (amount of parsed data, etc.) or accessing the @c object
+       *  field.
+       * @param data Pointer to first byte of data.
+       * @param size Size of @a data, in bytes.
+       *
+       * The netstring parser does not buffer any data.  Rather, it calls the
+       * callback with any consumed data as soon as it is made available by
+       * client code.  This means that this callback may be invoked several
+       * times for the same header.
+       */
+    void (*accept)(struct netstring_parser*, const char*, size_t);
+
+      /*!
+       * @brief Callback indicating the end of the netstring.
+       *
+       * This callback is invoked once, when the last chunk of data has been
+       * forwarded through @c accept().
+       */
+    void (*finish)(struct netstring_parser*);
+};
+
+  /*!
+   * @brief Initialize a parser and its limits.
+   */
+void netstring_setup
+    ( struct netstring_limits * limits, struct netstring_parser * parser );
+
+  /*!
+   * @brief Clear errors and reset the parser state.
+   *
+   * This function does not clear the @c object and callback fields.  You may
+   * call it to re-use any parsing context, such as allocated buffers for
+   * string contents.
+   */
+void netstring_clear ( struct netstring_parser * parser );
+
+  /*!
+   * @brief Feed data to the parser.
+   * @param data Pointer to first byte of data.
+   * @param size Size of @a data, in bytes.
+   * @return Number of bytes consumed.  Normally, this value is equal to
+   *  @a size.  However, the parser may choose to interrupt parser early or stop
+   *  processing data because of an error.
+   *
+   * You should @e always check the parser state after a call to this method.
+   * In particular, all data may be consumed before an error is reported, so
+   * a return value equal to @a size is not a reliable indicator of success.
+   */
+size_t netstring_consume ( const struct netstring_limits * limits,
+    struct netstring_parser * parser, const char * data, size_t size );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _netstring_h__ */
